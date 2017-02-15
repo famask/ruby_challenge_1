fa224ad822586d7476c682c162145522bea00816
famask <faisal.abdullah@gmail.com>
2017-02-15 16:24:30 +0500

    Refactored url_parser.rb

--- url_parser.rb
+++ url_parser.rb
@@ -1,3 +1,20 @@
+def parse_url(url) 
+  protocol = get_protocol(url)
+  domain = get_domain(url)
+  path = get_path(url)
+  query_params = get_query_params(url)
+
+  parsed_url = {}
+  parsed_url[:url] = url
+  parsed_url[:protocol] = protocol
+  parsed_url[:domain] = domain
+  parsed_url[:path] = path
+  parsed_url[:query_params] = query_params
+  
+  parsed_url
+end
+
+
 # "http://www.google.com/search?q=kittens"
 # Should return "http"
 # "ftp://cvcdd.com" should return "ftp"
@@ -5,10 +22,8 @@
 def get_protocol(url)
  url_partitioned_at_colon = url.partition(":")
  url_partitioned_at_colon[0]
-
 end
 
-get_protocol("ftp://cvcdd.com")
 
 
 # "http://www.google.com/search?q=kittens"
@@ -19,11 +34,8 @@ def get_domain(url)
   url_partitioned_at_double_hash = url.partition("//")
   domain_partitioned_at_hash = url_partitioned_at_double_hash[2].partition("/")
   domain_partitioned_at_hash[0]
-
 end
 
-get_domain("http://www.google.com/search?q=kittens")
-get_domain("ftp://cvcdd.com")
 
 # "http://www.google.com/search?q=kittens"
 # Should return "q=kittens"
@@ -34,8 +46,6 @@ def get_query_params(url)
   url_partitioned_at_question_mark[2]
 end
 
-get_query_params("http://www.google.com/search?q=kittens")
-get_query_params("ftp://cvcdd.com")
 
 #------------------Assignment 1--------------------------
 
@@ -56,19 +66,6 @@ def get_path(url)
   path_with_query[0]
 end
 
-
-
-# def get_path(url)
-#   require 'uri'
-#   url = URI.parse(url)
-#   url.path
-# end
-
-get_path("http://www.google.com/search/animals?q=kittens")
-get_path("https://docs.ruby-lang.org/en/2.1.0/URI.html")
-get_path("ftp://cvcdd.com")
-
-
 #------------------Assignment 2--------------------------
 
 # write another method (parse_url). It should print out URL, 
@@ -77,42 +74,6 @@ get_path("ftp://cvcdd.com")
 
 # Notes: String templating with method not working properly
 # therefore solved the assisnment using the long route.
-
-
-
-# def parse_url(url)
-#   protocol = url.partition(":")[0]
-#   domain = url.partition("//")[2].partition("/")[0]
-
-#   puts "Got url: #{url}"
-#   puts "Protocol: #{protocol}" 
-#   puts "Domain: #{domain}"
-#   puts "\n"
-
-# end
-
-# parse_url("http://www.google.com/search/animals?q=kittens")
-# parse_url("ftp://cvcdd.com")
-
-
-
-# def parse_url(url)
-#   protocol = get_protocol(url)
-#   domain = get_domain(url)
-#   path = get_path(url)
-#   query_params = get_query_params(url)
-
-#   puts "Got url: #{url}"
-#   puts "Protocol: #{protocol}" 
-#   puts "Domain: #{domain}"
-#   puts "Path: #{path}"
-#   puts "Query-Params: #{query_params}"
-#   puts "\n"
-
-# end
-
-# parse_url("http://www.google.com/search/animals?q=kittens")
-# parse_url("ftp://cvcdd.com")
 
 
 
@@ -130,22 +91,6 @@ get_path("ftp://cvcdd.com")
 #   protocol: 'http', domain: 'www.google.com', path: '/search/animals',
 # query_params: 'q=kittens'}
 
-
-
-
-def parse_url(url) 
-  parsed_url = {}
-  protocol = get_protocol(url)
-  domain = get_domain(url)
-  path = get_path(url)
-  query_params = get_query_params(url)
-  parsed_url[:url] = url
-  parsed_url[:protocol] = protocol
-  parsed_url[:domain] = domain
-  parsed_url[:path] = path
-  parsed_url[:query_params] = query_params
-  return parsed_url
-end
 
 puts "Hash table for #{"http://www.google.com/search/animals?q=kittens"}: #{parse_url("http://www.google.com/search/animals?q=kittens")}" 
 puts "\n"
